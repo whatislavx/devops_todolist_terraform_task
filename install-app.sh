9fe03a671325bc24b87dacc6effaf44f03e81a16
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2026-02-11 15:40:22 +0200

    Update virtual environment creation path in install script

--- install-app.sh
+++ install-app.sh
@@ -2,9 +2,9 @@
 
 apt-get update -yq
 apt-get install -yq python3 python3-venv python3-pip git
-python3 -m venv venv
 
 mkdir -p /app
+python3 -m venv /app/venv
 
 git clone https://github.com/whatislavx/devops_todolist_terraform_task.git /tmp/todolist
 cp -r /tmp/todolist/app/* /app
